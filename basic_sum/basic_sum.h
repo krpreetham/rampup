e4bcd9240eb0b66c513d91c015e6bcebfaafdbdb
preethamkr36 <preethamkr.xworkz@gmail.com>
2023-01-31 14:13:05 +0530

    all deleted

--- basic_sum/basic_sum.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BASIC_SUM__
-#define __BASIC_SUM__
-
-double add(double num1, double num2);
-double sub(double num1, double num2);
-double mul(double num1, double num2);
-double div(double num1, double num2);
-
-#endif /* __BASIC_SUM__ */
